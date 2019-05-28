3ffc5c32341fa57f9294de7880d24ce12217bbef
Syed Humair <syed.humair@gmail.com>
2019-05-28 16:28:31 -0400

    Updated Documentation on pypi

--- setup.py
+++ setup.py
@@ -13,13 +13,21 @@ try:
 except ImportError:
     from distutils.core import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
     name='S3-Inspect',
-    version='1.2',
+    version='1.3',
     description='A package to inspect contents of S3 buckets and generate report',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Syed Humair',
     author_email="syed.humair@gmail.com",
     keywords=['aws', 's3', 'challenge'],
